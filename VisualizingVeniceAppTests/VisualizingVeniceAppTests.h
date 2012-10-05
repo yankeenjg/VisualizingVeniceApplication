fed03c835202625bc80ee401e1c3c052d82a8e6d
Nick Gordon <nick.gordon@duke.edu>
2012-10-05 01:25:59 -0400

    Initial Commit

---
+++ VisualizingVeniceAppTests/VisualizingVeniceAppTests.h
@@ -0,0 +1,13 @@
+//
+//  VisualizingVeniceAppTests.h
+//  VisualizingVeniceAppTests
+//
+//  Created by Nicholas Gordon on 10/5/12.
+//  Copyright (c) 2012 Nicholas Gordon. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface VisualizingVeniceAppTests : SenTestCase
+
+@end
